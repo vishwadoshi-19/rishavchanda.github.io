f8c4b768b88bb6fe4636424b950cbde0bc31371d
Vishwa Doshi <vishwadoshi19@outlook.com>
2024-02-12 04:00:11 +0530

    initial commit

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -80,7 +80,7 @@ const index = () => {
             <Wrapper>
                 <Title>Experience</Title>
                 <Desc>
-                    My work experience as a software engineer and working on different companies and projects.
+                    No work experience as of now. 😔
                 </Desc>
                 <TimelineSection>
                     <Timeline>
